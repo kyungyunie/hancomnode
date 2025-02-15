e59f869cf7c6e8907534fb535973704cbc3b70b3
tommykim <tommynara@gmail.com>
2025-02-15 16:43:19 +0900

    crud add

--- users/models/index.js
+++ users/models/index.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'm95.cq1ymn0qmmdx.ap-northeast-2.rds.amazonaws.com',
-  user: process.env.DB_USER || 'masterchain',
-  password: process.env.DB_PASSWORD || 'mastercomes',
+  user: process.env.DB_USER || 'masterchain1',
+  password: process.env.DB_PASSWORD || '***********',
   database: process.env.DB_NAME || 'nodejs',
   waitForConnections: true,
   connectionLimit: 10,
